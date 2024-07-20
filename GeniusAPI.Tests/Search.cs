ba3687ad19fe0e0f3a3848baf4ff282cf3e65615
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-07-20 18:49:41 +0200

    Added unit tests

---
+++ GeniusAPI.Tests/Search.cs
@@ -0,0 +1,59 @@
+﻿#pragma warning disable IDE1006 // Naming Styles
+
+using GeniusAPI.Models;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace GeniusAPI.Tests;
+
+internal class Search
+{
+    ILogger<LyricsClient> logger;
+    LyricsClient client;
+
+    [SetUp]
+    public void Setup()
+    {
+        ILoggerFactory factory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        });
+
+        logger = factory.CreateLogger<LyricsClient>();
+        client = new(TestData.AccessToken, logger);
+    }
+
+
+    [Test]
+    public void search_for_tracks()
+    {
+        IEnumerable<LyricsTrack>? tracks = null;
+
+        Assert.DoesNotThrowAsync(async () =>
+        {
+            tracks = await client.SearchTracksAsync(TestData.Query);
+        });
+        Assert.That(tracks, Is.Not.Null);
+        Assert.That(tracks, Is.Not.Empty);
+
+        // Output
+        logger.LogInformation("\nTracks: {tracks} ", JsonSerializer.Serialize(tracks, TestData.SerializerOptions));
+    }
+
+
+    [Test]
+    public void fetch_track_lyrics()
+    {
+        string? trackLyrics = null;
+
+        Assert.DoesNotThrowAsync(async () =>
+        {
+            trackLyrics = await client.FetchLyricsAsync(TestData.Url);
+        });
+        Assert.That(trackLyrics, Is.Not.Null);
+        Assert.That(trackLyrics, Is.Not.Empty);
+
+        // Output
+        logger.LogInformation("\nTrack Lyrics: {trackLyrics} ", trackLyrics);
+    }
+}
---
+++ GeniusAPI.Tests/TestData.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace GeniusAPI.Tests;
+
+internal class TestData
+{
+    public static JsonSerializerOptions SerializerOptions = new()
+    {
+        WriteIndented = true
+    };
+
+
+    public const string AccessToken = "u_s2DsG-ewN4YDxgLZxzpo01mZaWSePOilc5rkBcylAYZ29cl93UzA7OEuPxWOCr";
+
+    public const string Query = "Pashanim";
+
+    public const string Url = "https://genius.com/Pashanim-shababs-botten-lyrics";
+}
